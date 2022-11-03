1ae1fa7e52cc8c087a46f9153e62abf6f58d2824
Uber <96502613+uberripper@users.noreply.github.com>
2022-11-03 20:26:13 +0000

    #2 Add, remove, edit, find by name and address added (#2)
    functionality added:
     * add item
     * edit item
     * delete item
     * find All By Name
     * find All By Location Address
     * find all by location and name (aka find a wrench at my home)

--- src/main/kotlin/com/uberripper/shitApi/repositories/ItemRepo.kt
+++ src/main/kotlin/com/uberripper/shitApi/repositories/ItemRepo.kt
@@ -3,10 +3,19 @@ package com.uberripper.shitApi.repositories
 import com.uberripper.shitApi.objects.Items
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
 
 interface ItemRepo : MongoRepository<Items, String> {
     fun findOneById(id: ObjectId): Items
-    override fun deleteAll()
 
+    fun findAllByName(name: String): List<Items>
+
+    @Query("{'location.locAddress' : '?0'}")
+    fun findAllByLocationLocAddress(address: String): List<Items>
+
+    @Query("{'name': '?0', 'location.locAddress' : '?1'}")
+    fun findAllByLocationLocAddressAndName(address: String, name: String): List<Items>
+
+    override fun deleteAll()
 }
